f4bce740bd9bf8876d0e30bb0e8c8e39117edc35
Rob Coe <robcoe18@gmail.com>
2020-11-01 11:26:35 -0800

    Test change
    Test change

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -41,6 +41,7 @@ public class ExampleCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    System.out.println("End Example");
   }
 
   // Returns true when the command should end.
