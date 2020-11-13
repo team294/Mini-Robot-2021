f4875cd89845cf0c3c00f6df4cd211f59e2a7438
Rob Coe <robcoe18@gmail.com>
2020-11-13 12:30:13 -0800

    Revert "updated comments"
    This reverts commit 67064f9532423ee1b90c7853bc3fb312cf54d016.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -44,29 +44,24 @@ public class Shooter extends SubsystemBase {
   }
 **/
   public void getTimeDelay(){  
-
-    /*  This method measures the time delay between when a ball breaks the beam of two sensors
     
-          // This is for testing sensors only run every 20 msec
-    SmartDashboard.putBoolean("Detector 1", !inputA.get());   // comment it out so it doesn't waste resources
+    ball =  !inputA.get();      // This is for testing sensors only run every 20 msec
+    SmartDashboard.putBoolean("Detector 1",ball);   // comment it out so it doesn't waste resources
     SmartDashboard.putBoolean("Detector 2", !inputB.get());
  
     
-    startTime = inputA.readFallingTimestamp();  // Time since program started
-    endTime   = inputB.readFallingTimestamp();
+      startTime = inputA.readFallingTimestamp();
+      endTime   = inputB.readFallingTimestamp();
 
-    if((startTime > lastEndTime) && (endTime > startTime)){  // new Ball
-      ++shotCount;
-  
-      System.out.println("Start Time " + startTime + ":    End Time " +endTime);                
-        
-      timeDelay = endTime - startTime;
-        // calculate velocity  for 3 inch separation of sensors
-      //velocity = 1/( 4 * timeDelay);   // result in ft/sec
-
-      // calculate velocity  for 1.5 inch separation of sensors
-      velocity = 1/( 8 * timeDelay);   // result in ft/sec
+      if((startTime > lastEndTime) && (endTime > startTime)){  // new Ball
+        ++shotCount;
    
+        System.out.println("Start Time " + startTime + ":    End Time " +endTime);                
+         
+        timeDelay = endTime - startTime;
+          // calculate velocity  for 3 inch separation of sensors
+        velocity = 1/( 4 * timeDelay);   // result in ft/sec
+    //  }
     }
     lastEndTime = endTime;
  
